eb7358121daeed583db267e128f5220f29e0e336
Brooklyn <marcusdev13@gmail.com>
2022-11-05 08:38:28 -0300

    Alteracoes na repeticao

--- repeticao2.php
+++ repeticao2.php
@@ -1,5 +1,11 @@
 <?php
 
-for ($contador = 1; $contador <=15; $contador = $contador + 1) {
-    echo "#$contador" . PHP.EOL;
+for ($contador = 1; $contador <=15; $contador++) {
+    if ($contador == 13) {
+        continue;
+    }
+    echo "#$contador" . PHP_EOL;
 }
+
+//Continue pula uma iteração de um laço
+//Break finaliza o laço
